12ab410bcb7cde7e342c066ebd2b23eca524b610
Katye Russell <katye.russell@gmail.com>
2015-03-01 01:09:26 -0600

    Added submarine.png to img file. Broke some functions into smaller pieces for modularization.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,103 +1,58 @@
 'use strict';
 
 var fb = new Firebase('https://xogame.firebaseio.com/');
-var boardfb = [['b','b','b','b','b'],['b','b','b','b','b'],['b','b','b','b','b'],['b','b','b','b','b'],['b','b','b','b','b']];
+var board = [['b','b','b','b','b'],['b','b','b','b','b'],['b','b','b','b','b'],['b','b','b','b','b'],['b','b','b','b','b']];
 //var board2 = [['','','','',''],['','','','',''],['','','','',''],['','','','',''],['','','','','']];
-var subImg = '<img src="http://www.clipartlord.com/wp-content/uploads/2013/03/submarine.png" height="100px" width="100px">';
+var subImg = '<img src="/img/submarine.png">';
 var sub = "sub";
-var bShip = "bShip"
 var shipNumber = 0;
 var coords;
 
-
 $('button').on('click', function(){
-  createBoard(boardfb);
-})
+  setBoard();
+});
 
 
-// Creates initial board array in FB and on page
-function createBoard (tableData) {
-  var $table = $('<table></table>');
-  tableData.forEach(function (row) {
-    var $tr = $('<tr></tr>');
-    row.forEach(function (cell){
-      $tr.append($('<td></td>'));
-      $table.append($tr);
-    });
-  });
-  //$('.board').append($table);
-  // Currently sets up to 3 submarines on board, updates board
-  // and appends an image of  a submarine in the clicked td
-  findCoords();
-}
-
-//  Called as part of 'findCoords' updates the firebase array
-//  with the ship's coordinates (as each ship is added).
-//   !!  currently only allows placement of submarines !!
-function updateBoardArray(coords, board) {
-	if(shipNumber <= 3){
-      board[coords.row][coords.col] = sub;
-      fb.child('/Game').child('/board').set(board);
-	}
-	//$('table').replaceWith(createBoard(board));
-}
-
-// Identifies where ships have been added to the board
-function findCoords(){
-	// on click of a square
+function setBoard(){
   $('td').one('click', function(e){
-
-  	//finds the square's coordinates so a ship can go there
-  	var coords = findIndex(e.target);
-  	console.log(coords);
-
-  	//iterates (and should limit) the number of ships added to boad
-    limitShips(coords);
-
-    // updates the board array data in firebase
-    updateBoardArray(coords, boardfb);
-
-    // updates player1's board so ships are visible
-    // !! currently roates through and double/triple appends
-    // !! sub images to the previously labeled (and already 'subbed')
-    // !! cells -- need to do a 'forEach' possibly?
-    updatePlayingBoard(boardfb);
+ 	var coords = findIndex(e.target);
+    shipNumber+=1;
+    if (shipNumber <= 3) {
+    updateBoardArray(coords, board);
+    // Still need an appending function here
+    }
   });
 }
 
-// iterates number of ships that have been added to the board
-// Needs to have limit feature added back to the function
-function limitShips() {
-	shipNumber += 1;
-	//if (shipNumber <= 3) {attachShip($(this));}
+/////////////////////////////////////////////////////
+//                                                 //
+//  The append function I was using was a mod. of  //
+//  Allison's tic-tac-toe code, like this:         //
+//                                                 //
+//  var cell = board[row][col];                    //
+//  var cellClass = '.cell-' + row + '-' + col;    //
+//  $(cellClass).append(subImg);                   //
+//                                                 //
+//  I'd like to make it work with a fb.snapshot    //
+//  so we're pulling from Firebase to update the   //
+//  board displays for player1 & player2, but not  //
+//  really sure (yet) how to make that work.       //
+//                                                 //
+/////////////////////////////////////////////////////
+
+
+// updates the board array data in firebase
+function updateBoardArray(coords, board) {
+    board[coords.row][coords.col] = sub;
+    fb.child('/Game').child('/board').set(board);
 }
 
-
-// Takes data from the current firebase board
-// and makes placed ships visible on player1's board 
-// !! see notes above about double/triple appending issue !!
-function updatePlayingBoard(board) {
-    for(var row=0; row < 5; row++) {
-        for(var col=0; col < 5; col++) {
-            var cell = board[row][col];
-            var  cellClass = '.cell-' + row + '-' + col;
-
-            if (cell === 'sub') {
-              $(cellClass).append(subImg);
-            }
-        }
-    }
-}
-
-// not sure this is still needed (or being called anywhere)
+//finds the square's coordinates so a ship can go there
 function findIndex(element) {
   var row = $(element).closest('tr').index(),
       col = $(element).index();
-  return { row: row, col: col }
+  return { row: row, col: col };
+  // need to figure out how to parse this returned data
+  // so it works for the Allison-style append I want to do.
 }
 
-// Deprecated by 'updatePlayingBoard' function
-///////////////////////////////////////////////
-// function attachShip(td) {
-//   td.append(ship);
-// }
