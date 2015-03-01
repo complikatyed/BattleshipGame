f64f784d0b204c73ce1b78da83afa05467a4bea4
Katye Russell <katye.russell@gmail.com>
2015-03-01 15:49:56 -0600

    Changed array structure, reworked fb update function, added project goals as comments.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,79 +1,122 @@
 'use strict';
 
 var fb = new Firebase('https://xogame.firebaseio.com/');
+var fb2 = new Firebase('https://xogame.firebaseio.com/Games');
 var emptyBoard = [['b','b','b','b','b'],['b','b','b','b','b'],['b','b','b','b','b'],['b','b','b','b','b'],['b','b','b','b','b']];
+var emptyBoard2 = ['b','b','b','b','b','b','b','b','b','b','b','b','b','b','b','b','b','b','b','b','b','b','b','b','b'];
+
 var subImg = '<img src="/img/submarine.png">';
 var sub = "sub";
 var shipNumber = 0;
 var coords;
 var uuid;
+var p1Board;
 
-$('button').on('click', function(){
-  createNewGame();
-  //testStuff();
+$('.create').on('click', function(){
+  createNewGame();   // consider adding a game array that uuids would push into
 });
 
+$('.set').on('click', function(){
+  setShips();
+});
+
+
+// Creates new game with empty boards & ship/points counters for each players
 function createNewGame() {
   var fb2 = new Firebase('https://xogame.firebaseio.com/Games');
   var newGameRef = fb2.push();
   var uuid = newGameRef.key();
- 
-  newGameRef.set({p1Board: emptyBoard, p2Board: emptyBoard, p1Ships: '', p2Ships: '', p1Points: 0, p2Points: 0})
-
+  newGameRef.set({p1Board: emptyBoard2, p2Board: emptyBoard2, p1Ships: '', p2Ships: '', p1Points: 0, p2Points: 0})
+  return uuid;
 }
 
+// Trying a new version of setShips function
 
-
-
-
-function setBoard(){
-
+function setShips() {
   $('td').one('click', function(e){
- 	var coords = findIndex(e.target);
-    shipNumber+=1;
-    if (shipNumber <= 3) {
-    updateBoardArray(coords, board);
-    detectShips();
-    // Still need an appending function here
-    }
+  var index = $(e.target).attr('id');
+  var uuid = '-JjLsgULrOzhuU0VEjpd';  // need this to be pulling, not hard coded
+
+  updateP1Board(uuid, index, sub);   // Want to change this so the ship can be changed by user.
   });
 }
 
+// updates player1's board-array in firebase
+// index is the cell to be marked with a ship
+// uuid is the identifier for the game being used
+// ship will equal the type of ship (or portion of a ship) being added
+function updateP1Board(uuid, index, ship) {
+  fb2.child(uuid).child('p1Board').child(index).set(ship);
+}
+
 /////////////////////////////////////////////////////
 //                                                 //
-//  The append function I was using was a mod. of  //
-//  Allison's tic-tac-toe code, like this:         //
+//  GOAL 1: append ship data to display            //
 //                                                 //
-//  var cell = board[row][col];                    //
-//  var cellClass = '.cell-' + row + '-' + col;    //
+//  The append function I was using yesterday      //
+//  looked like this:                              //
+//                                                 //
 //  $(cellClass).append(subImg);                   //
 //                                                 //
-//  I'd like to make it work with a fb.snapshot    //
+//  The goal is to pull the data w a fb.snapshot   //
 //  so we're pulling from Firebase to update the   //
-//  board displays for player1 & player2, but not  //
-//  really sure (yet) how to make that work.       //
+//  board display for player1 (and eventually p2)  //
+//                                                 //
+/////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////
+//                                                 //
+//  GOAL 2: allow user to choose ships             //
+//                                                 //
+//  This is going to require additional buttons    //
+//  and probably a multi-step process for the      //
+//  larger (multi-cell) ships.                     //
+//                                                 //
+//  Needs to feed the ship data into 'setShips'    //
+//  so the correct ship data is added to array     //
+//                                                 //
+/////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////
+//                                                 //
+//  GOALS 3+                                       //
+//                                                 //
+//  3. Display player 2 board (w no ships) to p1   //
+//                                                 //
+//  4. Switch turns between players                //
+//                                                 //
+//  5. Track hits & misses (similar to setShips)   //
+//                                                 //
+//  6. Change color of square based on hit/miss    //
+//                                                 //
+//  7. Track points based on hits (store in FB)    //
+//                                                 //
+//  8. Track remaining ships (based on hits)       //
+//                                                 //
+//  9. Add bonus points for sinking whole ship?    //
+//                                                 //
+//  10. Determine game over & winner (by points)    //
 //                                                 //
 /////////////////////////////////////////////////////
 
 
-// updates the board array data in firebase
+/////////////////////////////////////////////////////
+//                                                 //
+//  The code below has been deprecated by reworks  //
+//                                                 //
+/////////////////////////////////////////////////////
+
+// Replaced by 'updateP1Board'
 function updateBoardArray(coords, board) {
     board[coords.row][coords.col] = sub;
     fb.child('/Games').child('/uuid').set(board);
 }
 
-function detectShips(){
-  var fb2 = new Firebase('https://xogame.firebaseio.com/Game/board');
-  
-}
-
-//finds the square's coordinates so a ship can go there
+// Replaced by 'setShipIndex'
 function findIndex(element) {
   var row = $(element).closest('tr').index(),
       col = $(element).index();
   console.log(row, col);
   return { row: row, col: col };
-  // need to figure out how to parse this returned data
-  // so it works for the Allison-style append I want to do.
 }
 
