7b5028c69e825fe88e4e7cb3fb7d1875a470222c
Andy Meagher <ameagher311@gmail.com>
2015-03-02 13:47:51 -0600

    to test you can only play as player2 so refresh after creating newgame as player1, but app now knows if you hit 3 ships and alerts winner

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var fb = new Firebase('https://battleshippractice.firebaseio.com/');
-var emptyBoard = [['','','','',''],['','','','',''],['','','','',''],['','','','',''],['','','','','']];
 var board = [['','','','',''],['','','','',''],['','','','',''],['','','','',''],['','','','','']];
 var otherBoard;
-var ship = '<img src="http://www.clipartlord.com/wp-content/uploads/2013/03/submarine.png" height="100px" width="100px">';
+var ship = 'X';
 var shipNumber = 0;
 var player1 = true;
-//var boardObj = _.zipObject(['row1', 'row2', 'row3', 'row4', 'row5'], board);
 var thisGameUUID;
+var hits = 0;
+
 
 //takes the last firebase object. If there is no player 2, you join as player 2. If there is a player 2, create new game.
 
@@ -19,10 +19,10 @@ $('.start').on('click', function(){
 	  var key = data && Object.keys(data)[0] || null;
 	  var game = data && data[key] || null;
 	  if (!data || game.p1 && game.p2) {
-	    var gameRef = fb.child('/Games').push({p1: 'player1'});
+	    var gameRef = fb.child('/Games').push({winner: false, p1: 'player1'});
 	    thisGameUUID = gameRef.key();    
 	  } else {
-	    fb.child('/Games').child(key).update({p1: game.p1, p2: 'player2'});
+	    fb.child('/Games').child(key).update({p2: 'player2'});
 	    thisGameUUID = fb.child('/Games').child(key).key();
 	    player1 = false;
 	  }
@@ -32,10 +32,33 @@ $('.start').on('click', function(){
 
 //the Play Someone! button pushes respective board to firebase. It needs to append board1 or board2 object as a table next to it
 //alerts if you didn't use all your ships
-$('.play').on('click',function(){
-  if(shipNumber >= 3){sendBoardtoFB(); grabSecondBoard(); createBoard(emptyBoard);}
+$('.send').on('click',function(){
+  if(shipNumber >= 3){sendBoardtoFB(); grabSecondBoard();}
   else{alert('you still have' + ' ' + (3 - shipNumber) + ' ' + 'ships to use!');}
+  $('.send').hide();
 });
+
+$('.play').on('click', function(){
+  if(player1 == true){alert('You have no one to play with, try waiting and clicking Play again');}
+  else {createBoard(otherBoard); assessMove();}
+});
+
+function checkWinner(){
+  fb.child('/Games').child(thisGameUUID).on('value', function(res){
+    var data = res.val();
+    if(data.winner.winner == 'player1'){alert('player1 Won the Game');}
+    else if(data.winner.winner == 'player2'){alert('player2 Won the Game');}
+  });
+}
+
+function assessMove(){
+	$('td').on('click', function(e){
+	  if($(this).hasClass('X')){hits += 1;}
+	  if(hits >= 3 && player1 == true){fb.child('/Games').child(thisGameUUID).child('/winner').update({winner: 'player1'});}
+	  else if(hits >= 3 && player1 == false){fb.child('/Games').child(thisGameUUID).child('/winner').update({winner: 'player2'});}
+	});
+	checkWinner();
+}
 
 //loops through array to create empty table
 function createBoard (tableData) {
@@ -43,7 +66,7 @@ function createBoard (tableData) {
   tableData.forEach(function (row) {
     var $tr = $('<tr></tr>');
     row.forEach(function (cell){
-      $tr.append($('<td></td>'));
+      $tr.append($('<td class=' + cell + '></td>'));
       $table.append($tr);
     });
   });
@@ -93,14 +116,5 @@ function grabSecondBoard(){
   }
 }
 
-//a way to select second table on page is $('table').eq(1)
-//var rows = $('table').eq(1).find('tr'); ==finds all the rows in second table
-//var cells = $('table').eq(1).find('td'); = finds all the td's in second table
-function appendOpponentsLocation(){
-	rows.forEach(function(){
-
-	})
-  
-}
 
 
